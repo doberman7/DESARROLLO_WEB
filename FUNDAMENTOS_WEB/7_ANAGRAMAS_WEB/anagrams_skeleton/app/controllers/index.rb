baa3659655f6da82058593393e899d3a92192c9c
doberman7 <ivanrubioangeles@gmail.com>
2017-05-24 13:21:08 -0500

    ANAGRAMAS WEB: conflicto al crear BD, rak abortado, Errno::ENOENT: No such file or directory - createdb

--- FUNDAMENTOS_WEB/7_ANAGRAMAS_WEB/anagrams_skeleton/app/controllers/index.rb
+++ FUNDAMENTOS_WEB/7_ANAGRAMAS_WEB/anagrams_skeleton/app/controllers/index.rb
@@ -1,13 +1,15 @@
+#por metodo GET ir a por default ir index.erb
 get '/' do
   erb :index
 end
 
+#con GET ir a word.erb y asignar el hash {:new_word => input del usuario}
 get '/:new_word' do
 	@new_word = params[:new_word]
 
   erb :word
 end
-
+#com POST redireccionar a :user_word
 post '/new_word' do
   redirect to ("/#{params[:user_word]}")
 end
