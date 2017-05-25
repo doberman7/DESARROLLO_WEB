bef6595c6c9578a5a84ee96a7728d35c5c14e590
codeamac19 <codeamac19@gmail.com>
2017-05-24 21:17:51 -0500

    ANAGRAMAS WEB: update, revisar el sembrado, esta haciendo un rollback

--- FUNDAMENTOS_WEB/7_ANAGRAMAS_WEB/anagrams_skeleton/app/models/word.rb
+++ FUNDAMENTOS_WEB/7_ANAGRAMAS_WEB/anagrams_skeleton/app/models/word.rb
@@ -1,12 +1,16 @@
 class Word < ActiveRecord::Base
   # Remember to create a migration!
 	#agrega a tu modelo un método parecido al que usaste en el ejercicio anterior de anagramas.
-	def canonical(word)
-		word.split(//).sort.join
+	def canonical_transf(word)
+		 word.split(//).sort.join
 	end
 	#método que utilizando la base de datos genere una lista de anagramas para una palabra
-	def self.canonical(word)
-		word.split(//).sort.join
+	def self.canon(word)
+		angrams = []
+		Word.where(canonical_version: word.canonical_transf) do |w|
+			angrams << w
+		end
+		puts angrams
 	end
 
 end
