c39fb6e34052c2f6dbde98b9f9493be744616f67
codeamac19 <codeamac19@gmail.com>
2017-05-26 19:50:37 -0500

    BARRA DE NAV FIJA: resuelto

--- FUNDAMENTOS_WEB/9_BARRA_NAV_FIJA/fixed_navigation/navbar.js
+++ FUNDAMENTOS_WEB/9_BARRA_NAV_FIJA/fixed_navigation/navbar.js
@@ -1,14 +1,11 @@
-//This is to prevent any jQuery code from running before the document is finished loading (is ready).
-$(function(){
-  // WINDOW is the super global object in client-side JavaScript, all the functions and variables that are defined in the global context are methods and properties of the window object.
-  //Bind an event handler to the "scroll" JavaScript event, or trigger that event on an element.
-  $(window).on("scroll", function () {
-    // Aquí deberás escribir la lógica que modificará la barra
-    console.log($(window).scrollTop());
-  		if($(window).scrollTop() > 800 && $(window).scrollTop() < 850)
-  		{
-  			alert('mission accomplished');
-  		}
-      });
-  });
-)};
+var divs = $('.top-nav'),
+    limit = 60;  /* scrolltop value when opacity should be 0 */
+
+$(window).on('scroll', function() {
+   var st = $(this).scrollTop();
+
+   /* avoid unnecessary call to jQuery function */
+   if (st <= limit) {
+      divs.css({ 'opacity' : (1 - st/limit) });
+   }
+});
