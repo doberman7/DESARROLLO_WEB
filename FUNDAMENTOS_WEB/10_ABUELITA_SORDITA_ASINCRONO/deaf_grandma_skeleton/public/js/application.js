5ce5e8d30e77a045eb150b3de63050b1aaf1bc11
doberman7 <ivanrubioangeles@gmail.com>
2017-05-31 15:19:59 -0500

    ABUELITA_SORDITA_ASINCRONO: pruebas con .serialize method

--- FUNDAMENTOS_WEB/10_ABUELITA_SORDITA_ASINCRONO/deaf_grandma_skeleton/public/js/application.js
+++ FUNDAMENTOS_WEB/10_ABUELITA_SORDITA_ASINCRONO/deaf_grandma_skeleton/public/js/application.js
@@ -1,7 +1,10 @@
+// Este código corre después de que `document` fue cargado(loaded) completamente. Esto garantiza que si amarramos(bind) una función a un elemento de HTML este exista ya en la página.
 $(document).ready(function() {
-	// Este código corre después de que `document` fue cargado(loaded) 
-	// completamente. 
-	// Esto garantiza que si amarramos(bind) una función a un elemento 
-	// de HTML este exista ya en la página. 
-
+ 	//Bind an event handler to the "submit" JavaScript event, or trigger that event on an element.
+	$('#forma').submit(function(event){
+		//alert('alerta enlazada al formulario antes de que la solicitud sea enviada a la BD');
+		event.PrevenDefault();
+		// aqui
+		console.log( $( this ).serialize() );
+	});
 });
