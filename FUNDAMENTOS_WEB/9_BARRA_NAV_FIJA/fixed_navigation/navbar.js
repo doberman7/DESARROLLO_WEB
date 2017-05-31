bd53c847eab904ee9acad034287297895c7f82e1
codeamac19 <codeamac19@gmail.com>
2017-05-30 20:42:09 -0500

    BARRA NAV FIJA: funcionando chido

--- FUNDAMENTOS_WEB/9_BARRA_NAV_FIJA/fixed_navigation/navbar.js
+++ FUNDAMENTOS_WEB/9_BARRA_NAV_FIJA/fixed_navigation/navbar.js
@@ -1,18 +1,14 @@
-
+//target el objeto window en el scroll
   $(window).on('scroll', function() {
+    //asignar el scrollTop del window a un var
     var st = $(this).scrollTop();
-
-    var calc  = (1-st/50)
-
-    console.log(calc);
-     /* avoid unnecessary call to jQuery function */
-     if (calc >= '1') {
-        $('.top-nav').css({ 'opacity' : 1});
-
-     } else if (calc < '0') {
-       $('.top-nav').css({ 'opacity' : 0.5 })
+    // visualizar en la consola del INSPECTOR los valores del 'st'
+    console.log(st);
+    //SI 'st' es mayo a 80 disminuir la opacidad a la mitad, la cual acepta solo valores entre 1 y 0
+     if (st > '80') {
+        $('.top-nav').css({ 'opacity' : 0.5});
+    // SI 'st' es menor o igual a 80 la opacidad es 1 o sea default
+     } else if (st <= '80') {
+       $('.top-nav').css({ 'opacity' : 1 });
      }
-
-
-
   });
